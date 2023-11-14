c8bb1c7b5666bf4c2840826bd5ae3a6c2cd4efbe
Akemi <124843821+Akemi2005@users.noreply.github.com>
2023-11-15 00:21:35 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -97,3 +97,40 @@ rightArrow.addEventListener("click", nextSlide);
 // Показуємо перший слайд при завантаженні сторінки
 showSlide(currentIndex);
 activateNavPoint(currentIndex);
+
+//ЗАКРІПКА МЕНЮ
+
+const menu = document.getElementById("window");
+const offset = menu.offsetTop;
+let lastScrollTop = 0; // Змінна для зберігання попереднього значення scrollTop
+function fixMenu() {
+const currentScrollTop = window.pageYOffset;
+// Перевіряємо, чи перевищує поточний scrollTop відступ меню
+if (currentScrollTop >= offset) {
+menu.classList.add("fixed-menu");
+document.body.style.paddingTop = menu.offsetHeight + "px";
+// Змінюємо колір меню
+menu.style.backgroundColor = "#e2e4e8";
+} else {
+menu.classList.remove("fixed-menu");
+document.body.style.paddingTop = 0;
+// Повертаємо оригінальний колір меню
+menu.style.backgroundColor = ""; // Оце очищає стиль
+}
+// Перевірка напрямку скролу
+if (currentScrollTop < lastScrollTop) {
+// Якщо скролл вгору, то знову знімаємо клас "fixed-menu" та відступ для body
+menu.classList.remove("fixed-menu");
+document.body.style.paddingTop = 0;
+// Повертаємо оригінальний колір меню
+menu.style.backgroundColor = ""; // Оце очищає стиль
+}
+lastScrollTop = currentScrollTop; // Зберігаємо поточне значення scrollTop
+}
+// Додаємо обробник події scroll для виклику функції fixMenu при скролі
+window.addEventListener("scroll", fixMenu);
+
+// Обробник події DOMContentLoaded, який може виконатися після завантаження DOM
+document.addEventListener("DOMContentLoaded", () => {
+const menuHeight = menu.offsetHeight; // Отримуємо висоту меню
+});
